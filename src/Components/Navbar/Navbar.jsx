10dae7900f538e61689aa8d57a3d9038fafa509a
FerdinandJr <145202573+FerdinandJr@users.noreply.github.com>
2023-11-02 20:30:05 +0800

    prefinal

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -4,60 +4,59 @@ import logo from '../Assets/catdog_logo.png'
 import cart_icon from '../Assets/cart_icon.png'
 import { Link } from 'react-router-dom'
 import { ShopContext } from '../../Context/ShopContext'
+import {FaBars, FaTimes} from "react-icons/fa"
+import { useRef } from 'react'
 
 
 const Navbar = () => {
 
     const [menu, setMenu] = useState ("shop");
     const {getTotalCartItems} = useContext(ShopContext);
+    
+    const navRef = useRef();
+
+	  const showNavbar = () => {
+		navRef.current.classList.toggle(
+			"responsive_nav"
+		);
+    }
 
   return (
     
     <header>
-            <nav className="navbar">
-            <div className="nav-logo">
-                <Link style={{textDecoration: 'none'}} to='/'><img src={logo} alt="" /></Link>
-                <Link style={{textDecoration: 'none'}} to='/'><p>Barks & Meows Paradise</p></Link>
-            </div>
-            <ul className="ms-auto nav-menu">
-                <li className="nav-link" onClick={()=>{setMenu("shop")}}><Link style={{textDecoration: 'none'}} to='/'>HOME</Link>{menu==="shop"?<hr/>:<></>}</li>
-                <li className="nav-link" onClick={()=>{setMenu("dogs")}}><Link style={{textDecoration: 'none'}} to='/dogs'>DOG</Link>{menu==="dogs"?<hr/>:<></>}</li>
-                <li className="nav-link" onClick={()=>{setMenu("cats")}}><Link style={{textDecoration: 'none'}} to='/cats'>CAT</Link>{menu==="cats"?<hr/>:<></>}</li>
-                <div className="nav-login-cart">
+			<div className="nav-logo">
+        <Link style={{textDecoration: 'none'}} to='/'><img src={logo} alt="" /></Link>
+        <Link style={{textDecoration: 'none'}} to='/'><p>Barks & Meows Paradise</p></Link>
+      </div>
+			<nav ref={navRef}>
+      <div className='rightnav'>
+          
+          <li class="nav-link" onClick={()=>{setMenu("shop")}}><Link style={{textDecoration: 'none'}} to='/'>HOME</Link>{menu==="shop"?<hr/>:<></>}</li>
+          <li class="nav-link" onClick={()=>{setMenu("dogs")}}><Link style={{textDecoration: 'none'}} to='/dogs'>DOG</Link>{menu==="dogs"?<hr/>:<></>}</li>
+          <li class="nav-link" onClick={()=>{setMenu("cats")}}><Link style={{textDecoration: 'none'}} to='/cats'>CAT</Link>{menu==="cats"?<hr/>:<></>}</li>
+            <div className="nav-login-cart">
                 <Link to='/login'><button class="nav-link">Login</button></Link>
                 <Link to='/cart'><img src={cart_icon} alt="" className='cartmenu nav-link'/></Link>
                 <div className="nav-cart-count">{getTotalCartItems()}</div>
             </div>
-            </ul>
-               <div class="hamburger">
-                <span className="bar"></span>
-                <span className="bar"></span>
-                <span className="bar"></span>
-
-               </div>
-            </nav>
-            </header>
+            </div>
+				<button
+					className="nav-btn nav-close-btn"
+					onClick={showNavbar}>
+					<FaTimes />
+				</button>
+			</nav>
+			<button
+				className="nav-btn"
+				onClick={showNavbar}>
+				<FaBars />
+			</button>
+		</header>
     
   )
   
 }
 
-      window.onload=function(){
-        const hamburger = document.querySelector(".hamburger");
-        const navMenu = document.querySelector(".nav-menu");
-
-        hamburger.addEventListener("click", () => {
-          hamburger.classList.toggle("active");
-          navMenu.classList.toggle("active");
-        })
-      
-        document.querySelectorAll(".nav-link").forEach(n => n. 
-        addEventListener("click", () => {
-          hamburger.classList.remove("active");
-          navMenu.classList.remove("active");
-        })
-        )
-        };
 
 
 
